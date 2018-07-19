2ca8f3a49153c1ae1d23f54d8ab2e42b1712b9a3
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-19 02:35:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Add your functions and code here
+function  destructivelyAppendKitten(name) {
+  kittens.push(name)
+} 
+  
+function  destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+} 
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  
+}
+
+
+
+/*
+
+  describe('appendKitten(name)', function() {
+    it('appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      expect(appendKitten("Broom")).toEqual(["Milo", "Otis", "Garfield", "Broom"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
+
+  describe('prependKitten(name)', function() {
+    it('prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      expect(prependKitten("Arnold")).toEqual(["Arnold", "Milo", "Otis", "Garfield"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
+
+  describe('removeLastKitten()', function() {
+    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
+
+  describe('removeFirstKitten()', function() {
+    it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
+
+})
+*/
